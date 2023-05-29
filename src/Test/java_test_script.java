631c02b3daafbb28cbadd882d26708cc34e3f7e0
amanbarwal0644 <amanbarwal.ab@gmail.com>
2023-05-29 17:04:14 +0530

    cross browser testing selenium code

---
+++ src/Test/java_test_script.java
@@ -0,0 +1,41 @@
+package Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+
+public class java_test_script {
+
+
+
+
+
+
+	
+
+		
+		
+		@Test
+		public void titlecheck() throws MalformedURLException {
+			
+			
+			MutableCapabilities caps = new MutableCapabilities();
+			
+			WebDriver driver = new RemoteWebDriver(new URL("https://hub.browserstack.com/wd/hub"),caps);
+			
+			
+			driver.get("https://git-scm.com/download/win");
+			
+			//Assert.assertTrue(driver.getTitle().matches("Git - Downloading Package"));
+			
+		}
+		
+		
+	}
+
+	
+
